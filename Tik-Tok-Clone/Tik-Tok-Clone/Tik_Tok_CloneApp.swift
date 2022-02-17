7628f6c972eb779e4d8ddaa31267e255d68f5af8
Connor Black <connor.b645@gmail.com>
2022-02-17 21:21:05 +0000

    Initial Commit

---
+++ Tik-Tok-Clone/Tik-Tok-Clone/Tik_Tok_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tik_Tok_CloneApp.swift
+//  Tik-Tok-Clone
+//
+//  Created by Connor Black on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tik_Tok_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
