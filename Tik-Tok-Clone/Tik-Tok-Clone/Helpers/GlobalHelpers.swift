964b9a9134e8f6a0afbc2e2c5878ceceff6b0fd4
Davide Senatore <senatore.dvd@gmail.com>
2022-02-22 20:04:31 +0100

    Renamed usernames

--- Tik-Tok-Clone/Tik-Tok-Clone/Helpers/GlobalHelpers.swift
+++ Tik-Tok-Clone/Tik-Tok-Clone/Helpers/GlobalHelpers.swift
@@ -13,9 +13,9 @@ let exampleVideoURL3 = "https://assets.mixkit.co/videos/preview/mixkit-fireworks
 
 var exampleSong1 = Song(songName: "Wonderwall", artistName: "Liam Gallagher", songImageName: "Oasis")
 
-var exampleProfile1 = Profile(userName: "@dvd404", following: 350, followers: 1400, likes: 2000, imageName: "dvd404")
-var exampleProfile2 = Profile(userName: "blacksapps", following: 301, followers: 500, likes: 3010, imageName: "connor")
-var exampleProfile3 = Profile(userName: "vikingskullapps", following: 200, followers: 410, likes: 200, imageName: "ben")
+var exampleProfile1 = Profile(userName: "@404dvd", following: 350, followers: 1400, likes: 2000, imageName: "dvd404")
+var exampleProfile2 = Profile(userName: "@blacksapps", following: 301, followers: 500, likes: 3010, imageName: "connor")
+var exampleProfile3 = Profile(userName: "@vikingskullapps", following: 200, followers: 410, likes: 200, imageName: "ben")
 
 var exampleTikTok1 = TikTok(profile: exampleProfile1, postDescription: "Keep coding #swiftUI #swift", videoURL: exampleVideoURL1, previewImageName: "dvd404", song: exampleSong1, likes: 300, comments: 200, shareCounter: 500)
 var exampleTikTok2 = TikTok(profile: exampleProfile2, postDescription: "travel #2022", videoURL: exampleVideoURL2, previewImageName: "blacksapp", song: exampleSong1, likes: 400, comments: 100, shareCounter: 22)
